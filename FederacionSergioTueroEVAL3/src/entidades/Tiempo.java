c6fab893e3361dabf7eef032e5255ddc6fa58b90
sergi <sergi@LAPTOP-VJ4VNLSS>
2022-05-04 18:43:46 +0200

    Prueba commit examen 11

---
+++ FederacionSergioTueroEVAL3/src/entidades/Tiempo.java
@@ -0,0 +1,47 @@
+package entidades;
+
+public class Tiempo {
+	private int horas;
+	private int minutos;
+	private int segundos;
+	private int centesimas;
+	
+	public Tiempo(int horas, int minutos, int segundos, int centesimas) {
+		horas = this.horas;
+		minutos = this.minutos;
+		segundos = this.segundos;
+		centesimas = this.centesimas;
+	}
+	
+	public int getHoras() {
+		return horas;
+	}
+	public void setHoras(int horas) {
+		this.horas = horas;
+	}
+	public int getMinutos() {
+		return minutos;
+	}
+	public void setMinutos(int minutos) {
+		this.minutos = minutos;
+	}
+	public int getSegundos() {
+		return segundos;
+	}
+	public void setSegundos(int segundos) {
+		this.segundos = segundos;
+	}
+	
+	
+	public int getCentesimas() {
+		return centesimas;
+	}
+	public void setCentesimas(int centesimas) {
+		this.centesimas = centesimas;
+	}
+	@Override
+	public String toString() {
+		return this.horas + " : " + this.minutos + " : " + this.segundos + ", "+ this.centesimas;
+	}
+	
+}
